de7ca25f1c033e52c89b65df4d44249502e4d861
kkbrains1 <karen.hersschens@gmail.com>
2020-04-21 09:11:50 +0100

    vikings done

--- src/viking.js
+++ src/viking.js
@@ -12,7 +12,7 @@ class Soldier {
   }
 
   receiveDamage (damage) {
-    this.health = this.health - damage;
+    this.health -= damage;
   }
 }
 
@@ -40,7 +40,7 @@ class Viking extends Soldier {
 // Saxon
 class Saxon extends Soldier {
   receiveDamage (damage) {
-    this.health = this.health - damage;
+    this.health -= damage;
     if (this.health > 0) {
       return `A Saxon has received ${damage} points of damage`;
     } else {
